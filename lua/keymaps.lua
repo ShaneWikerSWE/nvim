7429bd706f367da459eb7bdc5622d30a706b74aa
Shane Wiker <shanewikerswe@gmail.com>
2023-01-28 16:50:01 -0800

    Auto Server Commit 28.01.2023 um 16:50

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -43,6 +43,7 @@ keymap("n", "<leader>ga", ":Git add .<CR>", opts) --Git add all
 keymap("n", "<leader>gb", ":Git branch -M ", opts) --Git select branch
 keymap("n", "<leader>gc", ":Git commit -m ", opts) --Git commit
 keymap("n", "<leader>gd", ":Gdiff", opts) --Git reset local changes
+keymap("n", "<leader>gg", ":GitGutterToggle", opts) --Toggle Git Gutter
 keymap("n", "<leader>gi", ":Git init<CR>", opts) --Git initialize repo
 vim.api.nvim_set_keymap('n', '<leader>gl',
 	'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>'
